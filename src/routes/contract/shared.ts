9db2f4bd7b8029211071e80d45275cf74984179b
kiuusai <kiuusai@protonmail.com>
2025-02-21 00:04:28 -0300

    Reconfiguring events numbers to be more consistent with expected features

--- src/routes/contract/shared.ts
+++ src/routes/contract/shared.ts
@@ -16,10 +16,11 @@ export interface ContractApprovalPayload {
 }
 
 // these events are non-standard, I made them up for the purposes of PLS
-// events with values between 1000 and 19999 are regular and can be retrieved any time
+// events with values between 1000 and 9999 are regular and can be retrieved any time
 // see https://nips.nostr.com/1#kinds
-export const ContractRequestEvent = 16970;
-export const ContractApprovalEvent = 16971;
+export const ContractRequestEvent = 1970;
 
+// events with values between 30000 and 39999 are addresssable and can be retrieved any time
+export const ContractApprovalEvent = 36971;
 // event to send encrypted DM
 export const SendEncryptedDM = 4;
