45c4122695db5f884a6b32c00b2f6dca198bea88
kiuusai <kiuusai@protonmail.com>
2025-02-21 00:28:52 -0300

    Fixing ContractApprovalEvent numbering

--- src/routes/contract/shared.ts
+++ src/routes/contract/shared.ts
@@ -19,6 +19,4 @@ export interface ContractApprovalPayload {
 // events with values between 1000 and 9999 are regular and can be retrieved any time
 // see https://nips.nostr.com/1#kinds
 export const ContractRequestEvent = 1970;
-
-// events with values between 30000 and 39999 are addresssable and can be retrieved any time
-export const ContractApprovalEvent = 36971;
+export const ContractApprovalEvent = 1971;
